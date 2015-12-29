6b0ee662a188434b82601e45d915ed7e86b3bc08
Andy Mention <amention@gmail.com>
2015-12-29 09:55:07 -0500

    Add Auth plug

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Shittalker.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Shittalker.Auth, repo: Shittalker.Repo
   end
 
   pipeline :api do
